84d8b13aaa1235a518b3a2783cba65cac36dab72
Carlos <carlos.gallego@adoptaunabuelo.com>
2021-12-23 13:02:05 +0100

    Tests for the font of text

--- src/test/Text.test.tsx
+++ src/test/Text.test.tsx
@@ -1,5 +1,6 @@
 import { render } from '@testing-library/react'
 import { P } from '../components';
+import { screen } from '@testing-library/react'
 
 
 describe("P", () => {
@@ -9,5 +10,29 @@ describe("P", () => {
                 Testing
             </P>
         );
+    })
+});
+describe("p_bold", () => {
+    it("render", () => {
+        render(
+            <P
+            weight={'bold'}
+            data-testid="p_bold">
+                Bold
+            </P>
+        );
+        expect(screen.queryByTestId("p_bold")).toHaveStyle(`fontWeight:700`);
+    })
+});
+describe("P_semibold", () => {
+    it("render", () => {
+        render(
+            <P
+            weight={'semibold'}
+            data-testid="p_semibold">
+                SemiBold
+            </P>
+        );
+        expect(screen.queryByTestId("p_semibold")).toHaveStyle(`fontWeight:600`);
     })
 });
