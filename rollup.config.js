3bd2f277bf26fda6691f3f94c8b7139d0999e4f0
Guillermo Angeles <guillermo.angeles@adoptaunabuelo.com>
2022-02-03 16:40:36 +0100

    idem

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,10 @@
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
-import dts from "rollup-plugin-dts";
 import postcss from "rollup-plugin-postcss";
+import dts from "rollup-plugin-dts";
+import { terser } from "rollup-plugin-terser";
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 
 const packageJson = require("./package.json");
 
@@ -22,20 +24,18 @@ export default [
       },
     ],
     plugins: [
+      peerDepsExternal(),
       resolve(),
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
-
-      // NEW
-      postcss(), 
+      postcss(),
+      terser(),
     ],
   },
   {
     input: "dist/esm/types/index.d.ts",
     output: [{ file: "dist/index.d.ts", format: "esm" }],
     plugins: [dts()],
-
-    // NEW
     external: [/\.css$/],
   },
 ];
