2e59f2861c1d7852562777e363c76f918b162ac1
Ronald <ronald.veliz@adoptaunabuelo.com>
2022-02-09 13:43:25 +0100

    defaultvalue

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -126,7 +126,7 @@ const Input = (props: Props) =>{
          //TEXT
         <><InputView {...props} data-testid="input">
         <IconStyle><User stroke={Color.gray2}/></IconStyle>
-        <InputStyled aria-label={props.label} {...props}  style={{fontSize:16,"paddingLeft":"64px", border:props.error ? `1px solid #FF5A5A`:value?"1px solid #00BA88":""}}/>
+        <InputStyled aria-label={props.label} onChange={onChange} defaultValue={value} style={{fontSize:16,"paddingLeft":"64px", border:props.error ? `1px solid #FF5A5A`:value?"1px solid #00BA88":""}}/>
         {value && <IconStyle onClick={()=>onValuechange("")} style={{right:16, cursor:"pointer"}}><X data-testid="close" stroke={Color.gray2}/></IconStyle>}
         </InputView>
         {error && <ErrorDiv>{error}</ErrorDiv>}
