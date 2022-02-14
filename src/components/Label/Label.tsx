d940ff2f3cb681e86cf04cd3de5f96057a31d777
Ronald <ronald.veliz@adoptaunabuelo.com>
2022-02-14 11:30:54 +0100

    new

--- src/components/Label/Label.tsx
+++ src/components/Label/Label.tsx
@@ -18,11 +18,56 @@ const LabelStyled = styled.div`
 `
 const Label = (props: Props) =>{
     return(
-        <LabelStyled data-testid="Label" id="Label" {...props} style={{background: props.backgroundColor ? props.backgroundColor:"red"}}>
-            <P style={{fontFamily: "Poppins",fontSize: "14px",fontStyle: "normal",fontWeight: "600",lineHeight: "22px",letterSpacing: "0em",textAlign: "left", "color":props.color}}>
-                {props.text}
+        <>
+        {props.text==="registered" ? 
+        <LabelStyled data-testid="Label" id="Label" {...props} style={{background:"#E5F1FC"}}>
+            <P style={{fontFamily: "Poppins",fontSize: "14px",fontStyle: "normal",fontWeight: "600",lineHeight: "22px",letterSpacing: "0em",textAlign: "left", "color":"#2D7FD9"}}>
+                Registrado
             </P>
-        </LabelStyled>
+        </LabelStyled>:
+        props.text==="match" ? 
+        <LabelStyled data-testid="Label" id="Label" {...props} style={{background:"#E7F6ED"}}>
+            <P style={{fontFamily: "Poppins",fontSize: "14px",fontStyle: "normal",fontWeight: "600",lineHeight: "22px",letterSpacing: "0em",textAlign: "left", "color":"#59C183"}}>
+                Matched
+            </P>
+        </LabelStyled>:
+        props.text==="shutdown" ? 
+        <LabelStyled data-testid="Label" id="Label" {...props} style={{background:"#FCEDF1"}}>
+            <P style={{fontFamily: "Poppins",fontSize: "14px",fontStyle: "normal",fontWeight: "600",lineHeight: "22px",letterSpacing: "0em",textAlign: "left", "color":"#FF5A5A"}}>
+                Baja
+            </P>
+        </LabelStyled>:
+        props.text==="paused" ? 
+        <LabelStyled data-testid="Label" id="Label" {...props} style={{background:"#F2F2F2"}}>
+            <P style={{fontFamily: "Poppins",fontSize: "14px",fontStyle: "normal",fontWeight: "600",lineHeight: "22px",letterSpacing: "0em",textAlign: "left", "color":"#828282"}}>
+                Pausado
+            </P>
+        </LabelStyled>:
+         props.text==="subscriptor" ? 
+         <LabelStyled data-testid="Label" id="Label" {...props} style={{background:"#FFEFD4"}}>
+             <P style={{fontFamily: "Poppins",fontSize: "14px",fontStyle: "normal",fontWeight: "600",lineHeight: "22px",letterSpacing: "0em",textAlign: "left", "color":"#FFAC4B"}}>
+                 Pendiente
+             </P>
+         </LabelStyled>:
+        props.text==="particular" ? 
+        <LabelStyled data-testid="Label" id="Label" {...props} style={{background: "#EBF9FF"}}>
+            <P style={{fontFamily: "Poppins",fontSize: "14px",fontStyle: "normal",fontWeight: "600",lineHeight: "22px",letterSpacing: "0em",textAlign: "left", "color":"#2D55B5"}}>
+                Particular
+            </P>
+        </LabelStyled>:
+        props.text==="residence" ? 
+        <LabelStyled data-testid="Label" id="Label" {...props} style={{background: "#EBECFF"}}>
+            <P style={{fontFamily: "Poppins",fontSize: "14px",fontStyle: "normal",fontWeight: "600",lineHeight: "22px",letterSpacing: "0em",textAlign: "left", "color":"#5963F6"}}>
+                Residencia
+            </P>
+        </LabelStyled>:
+         <LabelStyled data-testid="Label" id="Label" {...props} style={{background: props.backgroundColor ? props.backgroundColor:"red"}}>
+         <P style={{fontFamily: "Poppins",fontSize: "14px",fontStyle: "normal",fontWeight: "600",lineHeight: "22px",letterSpacing: "0em",textAlign: "left", "color":props.color}}>
+             {props.text}
+         </P>
+     </LabelStyled>
+        }
+        </>
     )
 }
 export default Label;
