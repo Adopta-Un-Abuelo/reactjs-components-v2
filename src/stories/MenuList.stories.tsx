92904d76d9cd09af4b90afc956f55c122eea42fc
Guillermo Angeles <guillermo.angeles@adoptaunabuelo.com>
2022-04-05 09:58:24 +0200

    Menu List edited

--- src/stories/MenuList.stories.tsx
+++ src/stories/MenuList.stories.tsx
@@ -1,26 +1,56 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import React from 'react';
 
-import { MenuList } from '../components';
+import { Package, Palette, Option } from 'lucide-react';
+import { Menu as MenuT, MenuList as MenuListT } from '../components';
 
 export default {
-	title: 'Basic/MenuList',
-	component: MenuList,
+	title: 'Basic/Menu',
+	component: MenuT,
     argTypes: {
 		position: { 
 			options: ['bottom-right', 'bottom-left', 'top-right', 'top-left'],
 			control: { type: 'select' }
-		}
+		},
+		onClick: { action: 'onClick' }
 	}
-} as ComponentMeta<typeof MenuList>;
+} as ComponentMeta<typeof MenuT>;
 
-const Template: ComponentStory<typeof MenuList> = (args) => (
+const Template: ComponentStory<typeof MenuT> = (args) => (
     <div style={{display: 'flex', flex: 1, height: '100vh', justifyContent: 'center', alignItems: 'center'}}>
-        <MenuList {...args}/>
+        <MenuT {...args}/>
     </div>
 );
 
-export const MenuListView = Template.bind({});
-MenuListView.args = {
-	id: 'menuList'
+export const Menu = Template.bind({});
+Menu.args = {
+	id: 'menu'
+}
+
+const Template2: ComponentStory<typeof MenuListT> = (args) => (
+    <div style={{display: 'flex', flex: 1, height: '100vh', justifyContent: 'center', alignItems: 'center'}}>
+        <MenuListT {...args}/>
+    </div>
+);
+
+export const MenuList = Template2.bind({});
+MenuList.args = {
+	id: 'menuList',
+	options: [
+		{
+			id: 'option1',
+			label: 'Option 1',
+			icon: Option
+		},
+		{
+			id: 'option2',
+			label: 'Option 2',
+			icon: Palette
+		},
+		{
+			id: 'option3',
+			label: 'Option 3',
+			icon: Package
+		}
+	]
 }
